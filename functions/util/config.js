3370581e31561fa9e05d05175ddaf8281695ac92
Carlos Habib <43607843+CarHab@users.noreply.github.com>
2019-07-26 20:01:54 -0300

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	apiKey: 'AIzaSyAZqkLlW64Fb2cLkOqVVjZy6PKGIC04k20',
-	authDomain: 'personalclub-23861.firebaseapp.com',
-	databaseURL: 'https://personalclub-23861.firebaseio.com',
-	projectId: 'personalclub-23861',
-	storageBucket: 'personalclub-23861.appspot.com',
-	messagingSenderId: '116547347880',
-	appId: '1:116547347880:web:2dd1c5c71983feb8'
+	apiKey: '',
+	authDomain: '',
+	databaseURL: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: ''
 }
